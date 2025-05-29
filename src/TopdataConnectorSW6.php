69353a3a38c7a64f5dc791bfc34de482ef0c7aea
Marc Christenfeldt <marc.christenfeldt@gmx.de>
2025-05-29 11:58:38 +0200

    refactor: extract table drop and column removal logic into helper methods

--- src/TopdataConnectorSW6.php
+++ src/TopdataConnectorSW6.php
@@ -51,37 +51,17 @@ class TopdataConnectorSW6 extends Plugin
         // ---- Get the database connection
         $connection = $this->container->get(Connection::class);
 
-        // ---- Drop plugin-related tables if they exist
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_brand`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_to_product`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_to_customer`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_to_synonym`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_type`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_series`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_to_product`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_alternate`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_similar`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_related`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_bundled`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_color_variant`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_capacity_variant`');
-        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_variant`');
+        $this->_dropPluginRelatedTables($connection);
+        $this->_removeColumnsFromCustomerTable($connection);
+        $this->_removeColumnsFromProductTable($connection);
 
-        // ---- Fetch and store customer table fields
-        $customerFields = [];
-        $temp = $connection->fetchAllAssociative('SHOW COLUMNS from `customer`');
-        foreach ($temp as $field) {
-            if (isset($field['Field'])) {
-                $customerFields[$field['Field']] = $field['Field'];
-            }
-        }
+    }
 
-        // ---- Drop `devices` field from `customer` table if it exists
-        if (isset($customerFields['devices'])) {
-            $connection->executeStatement('ALTER TABLE `customer` DROP `devices`');
-        }
-
+    /**
+     * 05/2025 TODO: I think this is not needed anymore (maybe some artefact from the sw5 version?)
+     */
+    private function _removeColumnsFromProductTable($connection): void
+    {
         // ---- Fetch and store product table fields
         $productFields = [];
         $temp = $connection->fetchAllAssociative('SHOW COLUMNS from `product`');
@@ -110,5 +90,49 @@ class TopdataConnectorSW6 extends Plugin
                 $connection->executeStatement('ALTER TABLE `product`  DROP `' . $field . '`');
             }
         }
+    }
+
+    /**
+     * 05/2025 // TODO: probably not needed anymore (probably some artefact from the sw5 version?)
+     */
+    private function _removeColumnsFromCustomerTable($connection): void
+    {
+        // ---- Fetch and store customer table fields
+        $customerFields = [];
+        $temp = $connection->fetchAllAssociative('SHOW COLUMNS from `customer`');
+        foreach ($temp as $field) {
+            if (isset($field['Field'])) {
+                $customerFields[$field['Field']] = $field['Field'];
+            }
+        }
+
+        // ---- Drop `devices` field from `customer` table if it exists
+        if (isset($customerFields['devices'])) {
+            $connection->executeStatement('ALTER TABLE `customer` DROP `devices`');
+        }
+    }
+
+    /**
+     * Drop plugin-related tables if they exist
+     *
+     * 05/2025 created (extracted from uninstall)
+     */
+    private function _dropPluginRelatedTables($connection): void
+    {
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_brand`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_to_product`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_to_customer`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_to_synonym`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_device_type`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_series`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_to_product`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_alternate`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_similar`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_related`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_bundled`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_color_variant`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_capacity_variant`');
+        $connection->executeStatement('DROP TABLE IF EXISTS `topdata_product_to_variant`');
     }
 }
